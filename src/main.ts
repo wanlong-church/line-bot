01ab68f63932d06465755a2347383060f2228a2e
lowking shih <76799909+lowkingshih@users.noreply.github.com>
2023-10-25 17:10:50 +0800

    Update main.ts
    - update version to 1.0.0
    - update tag to Alpha

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,8 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('萬隆 Line Bot API')
     .setDescription('API 說明文件')
-    .setVersion('1.0')
-    .addTag('0.1.0')
+    .setVersion('0.0.1')
+    .addTag('Alpha')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
