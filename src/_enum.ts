0aacf412fd063f20f990e8f81207bc9f028f8a8a
LowkingShih <mongrock30102@gmail.com>
2024-02-03 16:04:44 +0800

    feat: 沒啥卵用的 enum，先讓他活一陣子

---
+++ src/_enum.ts
@@ -0,0 +1,70 @@
+/** 辛苦打的，真的沒用到再刪掉 QAQ */
+//#region 服事表表頭
+// export enum SheetField {
+//   /** 日期 */
+//   DATE = 'date',
+//   /** 講員 */
+//   SPEAKER = 'speaker',
+//   /** 領會 */
+//   FACILITATOR = 'facilitator',
+//   /** 擘餅 */
+//   COMMUNION = 'communion',
+//   /** 司琴 */
+//   PIANIST = 'pianist',
+//   /** 合音1 */
+//   CHOIR_1 = 'choir1',
+//   /** 合音2 */
+//   CHOIR_2 = 'choir2',
+//   /** 吉他 */
+//   GUITAR = 'guitar',
+//   /** 貝士 */
+//   BASS = 'bass',
+//   /** 鼓 */
+//   DRUMS = 'drums',
+//   /** 投影片 */
+//   SLIDES = 'slides',
+//   /** 音控 */
+//   SOUND_CONTROL = 'soundControl',
+//   /** 直播 */
+//   LIVE_STREAM = 'liveStream',
+//   /** 兒主中年級 */
+//   YOUTH_LEADER_MIDDLE_GRADE = 'youthLeaderMiddleGrade',
+//   /** 幼兒主理 */
+//   NURSERY_COORDINATOR = 'nurseryCoordinator',
+//   /** 幼兒助理 */
+//   NURSERY_ASSISTANT = 'nurseryAssistant',
+//   /** 招待 */
+//   GREETER = 'greeter',
+//   /** 禱告會 */
+//   PRAYER_MEETING = 'prayerMeeting',
+//   /** 愛筵 */
+//   POTLUCK = 'potluck',
+//   /** 洗碗 */
+//   DISH_WASHING = 'dishWashing',
+//   /** 特別日 */
+//   SPECIAL_DAY = 'specialDay',
+// }
+// export const SheetFieldDisplayName: Record<SheetField, string> = {
+//   [SheetField.DATE]: '日期',
+//   [SheetField.SPEAKER]: '講員',
+//   [SheetField.FACILITATOR]: '領會',
+//   [SheetField.COMMUNION]: '擘餅',
+//   [SheetField.PIANIST]: '司琴',
+//   [SheetField.CHOIR_1]: '合音1',
+//   [SheetField.CHOIR_2]: '合音2',
+//   [SheetField.GUITAR]: '吉他',
+//   [SheetField.BASS]: '貝士',
+//   [SheetField.DRUMS]: '鼓',
+//   [SheetField.SLIDES]: '投影片',
+//   [SheetField.SOUND_CONTROL]: '音控',
+//   [SheetField.LIVE_STREAM]: '直播',
+//   [SheetField.YOUTH_LEADER_MIDDLE_GRADE]: '兒主中年級',
+//   [SheetField.NURSERY_COORDINATOR]: '幼兒主理',
+//   [SheetField.NURSERY_ASSISTANT]: '幼兒助理',
+//   [SheetField.GREETER]: '招待',
+//   [SheetField.PRAYER_MEETING]: '禱告會',
+//   [SheetField.POTLUCK]: '愛筵',
+//   [SheetField.DISH_WASHING]: '洗碗',
+//   [SheetField.SPECIAL_DAY]: '特別日子',
+// };
+//#endregion
